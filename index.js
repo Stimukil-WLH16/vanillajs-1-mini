e936605db24a2d14a240cc9a273d3160f9f644e7
unknown <jeff_cox76@hotmail.com>
2020-03-10 13:39:15 -0600

    in progress

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('hello, world')
+
+const count = 0;
+
+function increase(){
+    count++;
+    console.log(count)
+}
+
+function increase(){
+    count--;
+    console.log(count)
+}
+
+function reset(){
+    count = 0
+    console.log(count)
+}
